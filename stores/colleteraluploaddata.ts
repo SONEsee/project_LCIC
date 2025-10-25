6f65a0f5d1cd12bcfc61c81c4c0cfd9ac5683883
Sone11 <sonesdv164@gmail.com>
2025-10-25 15:26:06 +0700

    updat

---
+++ stores/colleteraluploaddata.ts
@@ -0,0 +1,165 @@
+import axios from "~/helpers/axios";
+import { defineStore } from "pinia";
+import { DispustCallateralModel,AlertCallateralModel } from "~/types";
+import Swal from "sweetalert2";
+export const useDipustCallateralStore = defineStore("dispust", {
+  state() {
+    return {
+      respons_data_dispust: null as DispustCallateralModel.Data | null,
+      respons_data_dispust_allert: null as AlertCallateralModel.Item |null,
+      pagination: null as AlertCallateralModel.Pagination | null,
+      isLoading: false,
+      data_fiter: {
+        isLoading: false,
+        query: {
+          id_file: "",
+          q: null as string | null,
+          page_size: 20,
+          page: 1,
+        },
+      },
+    form_create_dispust: {
+        file: null as File | null,
+        dispute_ids: [] as number[],
+        id_dispust: "" as string,
+        user_insert: "" as string,
+        user_id: "" as string,
+        deception: "" as string,
+      },
+      data_filter_dispust: {
+        isLoading: false,
+        query: {
+          bnk_code: "",
+          filter_bnk_code: "",
+          id_disput_loan: "",
+          page: 1,
+          page_size: 20,
+        },
+      },
+    };
+  },
+  actions: {
+    async getDataDispush() {
+      this.isLoading = true;
+      try {
+        const res = await axios.get<DispustCallateralModel.DispustCollateralRepues>(`/api/api/datac/`,{
+        params:{
+            ...this.data_fiter.query
+        }
+      });if(res.status ===200){
+        this.respons_data_dispust = res.data.data
+      }
+      } catch (error) {
+        Swal.fire({
+            icon:"error",
+            title:"ຜິດພາດ",
+            text:"ບໍ່ສາມາດດືງຂໍ້ມູນໄດ້"
+        })
+      }finally{
+        this.isLoading = false
+      }
+      
+    },
+        async createDispust() {
+      this.isLoading = true;
+      try {
+        if (!this.form_create_dispust.file) {
+          Swal.fire({
+            icon: "warning",
+            title: "ແຈ້ງເຕືອນ",
+            text: "ກະລຸນາເລືອກເອກະສານຢັ້ງຢືນ",
+          });
+          return;
+        }
+
+        if (this.form_create_dispust.dispute_ids.length === 0) {
+          Swal.fire({
+            icon: "warning",
+            title: "ແຈ້ງເຕືອນ",
+            text: "ກະລຸນາເລືອກລາຍການ Dispute ຢ່າງໜ້ອຍ 1 ລາຍການ",
+          });
+          return;
+        }
+
+        const formData = new FormData();
+        formData.append("file", this.form_create_dispust.file);
+        formData.append(
+          "dispute_ids",
+          JSON.stringify(this.form_create_dispust.dispute_ids)
+        );
+        formData.append("id_dispust", this.form_create_dispust.id_dispust);
+        formData.append("user_id", this.form_create_dispust.user_id);
+        formData.append(
+          "user_insert",
+          String(this.form_create_dispust.user_insert)
+        );
+
+        const res = await axios.post(`/api/api/disputes/confirmc/`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+
+        if (res.status === 201 || res.status === 200) {
+          await Swal.fire({
+            icon: "success",
+            title: "ສຳເລັດ",
+            text: res.data.message || "ບັນທຶກຂໍ້ມູນສຳເລັດ",
+            timer: 1500,
+            showConfirmButton: false,
+          });
+          setTimeout(() => {
+            goPreviousPath();
+          }, 1000);
+
+          
+
+          await this.getDataDispush();
+        }
+      } catch (error: any) {
+        console.error("Upload error:", error);
+
+        const errorMessage =
+          error.response?.data?.message ||
+          error.response?.data?.errors?.join(", ") ||
+          "ເກີດຂໍ້ຜິດພາດໃນການບັນທຶກຂໍ້ມູນ";
+
+        Swal.fire({
+          icon: "error",
+          title: "ຜິດພາດ",
+          text: errorMessage,
+        });
+      } finally {
+        this.isLoading = false;
+      }
+    },
+        async getDataDispust() {
+      this.isLoading = true;
+      this.data_filter_dispust.isLoading = true;
+      try {
+        const res = await axios.get<AlertCallateralModel.AllertDispustCallateralRepues>(
+          `/api/api/dispute-collateral/`,
+          {
+            params: {
+              ...this.data_filter_dispust.query,
+            },
+          }
+        );
+        if (res.status === 200) {
+          this.respons_data_dispust_allert = res.data.data.items;
+          
+        }
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: "ຜິດພາດ",
+          text: "ບໍ່ສາມາດດືງຂໍ້ມູນໄດ້.",
+        });
+      } finally {
+        this.isLoading = false;
+        this.data_filter_dispust.isLoading = false;
+      }
+    },
+
+  },
+});
---
+++ types/alertcollateral.ts
@@ -0,0 +1,38 @@
+export interface AllertDispustCallateralRepues {
+    status:          string;
+    data:            Data;
+    filters_applied: FiltersApplied;
+}
+
+export interface Data {
+    items:      Item;
+    pagination: Pagination;
+}
+
+export interface Item {
+    id_disput_loan: number;
+    bnk_code:       string;
+    image_url:      string;
+    user_insert:    string;
+    user_update:    null;
+    insertdate:     string;
+    updatedate:     null;
+    status:         string;
+    total:          number;
+}
+
+export interface Pagination {
+    page:         number;
+    page_size:    number;
+    total_items:  number;
+    total_pages:  number;
+    has_next:     boolean;
+    has_previous: boolean;
+}
+
+export interface FiltersApplied {
+    user_bnk_code:   string;
+    filter_bnk_code: string;
+    status:          string;
+    id_disput_loan:  string;
+}
