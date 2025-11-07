aa916c6b173b2d02b191682d909c2602c76f870f
tooktik <phonephaichitskd@gmail.com>
2025-11-07 13:31:14 +0700

    mergeeee

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,11 +37,7 @@ export default defineNuxtConfig({
     server: {
       proxy: {
         '/media': {
-<<<<<<< HEAD
           target: process.env.STRAPI_URL || 'http://192.168.45.55:8000',
-=======
-          target: process.env.STRAPI_URL || 'http://192.168.45.54:35729',
->>>>>>> cdf75b12df8c3388be768818ea05f1e7cbc242ab
           changeOrigin: true,
         }
       }
@@ -112,17 +108,10 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       strapi: {
-<<<<<<< HEAD
         url: process.env.STRAPI_URL || "http://192.168.45.55:8000/",
       },
      
       apiBase: process.env.STRAPI_URL || "http://192.168.45.55:8000",
-=======
-        url: process.env.STRAPI_URL || "http://192.168.45.54:35729/",
-      },
-     
-      apiBase: process.env.STRAPI_URL || "http://192.168.45.54:35729",
->>>>>>> cdf75b12df8c3388be768818ea05f1e7cbc242ab
     },
   },
   
@@ -130,11 +119,7 @@ export default defineNuxtConfig({
   nitro: {
     devProxy: {
       '/media': {
-<<<<<<< HEAD
         target: process.env.STRAPI_URL || 'http://192.168.45.55:8000',
-=======
-        target: process.env.STRAPI_URL || 'http://192.168.45.54:35729',
->>>>>>> cdf75b12df8c3388be768818ea05f1e7cbc242ab
         changeOrigin: true,
       }
     },
