540c19dba6ec0237cd7c8a034a673b42fc3d467d
tooktik <phonephaichitskd@gmail.com>
2025-11-14 16:19:01 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
     server: {
       proxy: {
         '/media': {
-          target: process.env.STRAPI_URL || 'http://192.168.45.54:35729',
+          target: process.env.STRAPI_URL || 'http://192.168.45.55:8000',
           changeOrigin: true,
         }
       }
@@ -108,10 +108,10 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       strapi: {
-        url: process.env.STRAPI_URL || "http://192.168.45.54:35729/",
+        url: process.env.STRAPI_URL || "http://192.168.45.55:8000/",
       },
      
-      apiBase: process.env.STRAPI_URL || "http://192.168.45.54:35729",
+      apiBase: process.env.STRAPI_URL || "http://192.168.45.55:8000",
     },
   },
   
@@ -119,7 +119,7 @@ export default defineNuxtConfig({
   nitro: {
     devProxy: {
       '/media': {
-        target: process.env.STRAPI_URL || 'http://192.168.45.54:35729',
+        target: process.env.STRAPI_URL || 'http://192.168.45.55:8000',
         changeOrigin: true,
       }
     },
