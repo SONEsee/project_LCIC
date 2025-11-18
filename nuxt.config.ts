5ea5711a0c873c5346a84b9b2996a9235430df9a
Sone11 <sonesdv164@gmail.com>
2025-11-18 13:58:39 +0700

    update all code in server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,11 +38,7 @@ export default defineNuxtConfig({
     server: {
       proxy: {
         '/media': {
-<<<<<<< HEAD
           target: process.env.STRAPI_URL ,
-=======
-          target: process.env.STRAPI_URL || 'http://192.168.45.56:8000',
->>>>>>> 41fe85c30db602749a65ebe1442c712ea08b73bb
           changeOrigin: true,
         }
       }
@@ -113,17 +109,10 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       strapi: {
-<<<<<<< HEAD
         url: process.env.STRAPI_URL ,
       },
      
       apiBase: process.env.STRAPI_URL,
-=======
-        url: process.env.STRAPI_URL || "http://192.168.45.56:8000/",
-      },
-     
-      apiBase: process.env.STRAPI_URL || "http://192.168.45.56:8000",
->>>>>>> 41fe85c30db602749a65ebe1442c712ea08b73bb
     },
   },
   
@@ -131,11 +120,7 @@ export default defineNuxtConfig({
   nitro: {
     devProxy: {
       '/media': {
-<<<<<<< HEAD
         target: process.env.STRAPI_URL ,
-=======
-        target: process.env.STRAPI_URL || 'http://192.168.45.56:8000',
->>>>>>> 41fe85c30db602749a65ebe1442c712ea08b73bb
         changeOrigin: true,
       }
     },
