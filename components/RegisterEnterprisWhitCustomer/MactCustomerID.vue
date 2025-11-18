37d31c3b8b8d67ee1657ba9d7037d0f05fd8cae1
Sone11 <sonesdv164@gmail.com>
2025-11-18 15:17:16 +0700

    updaste

--- components/RegisterEnterprisWhitCustomer/MactCustomerID.vue
+++ components/RegisterEnterprisWhitCustomer/MactCustomerID.vue
@@ -2,5 +2,14 @@
 
 </script>
 <template>
-    
+     <v-sheet
+    class="  flex-wrap  mx-auto px-4"
+    elevation="4"
+   
+    rounded
+  >
+    <div class="pa-4">
+        <h3>ພູກລະຫັດລູກຄ້າ</h3>
+    </div>
+  </v-sheet>
 </template>
